97fed1cb8b67fde544461d4e4071b871f442f286
Jason <jconn99801@gmail.com>
2023-08-07 12:13:58 -0800

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const submitData = (name, email) => {
+  const data = {
+    name: name,
+    email: email,
+  };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const id = data.id;
+      document.body.innerHTML += `<p>${id}</p>`;
+      return data;
+    })
+    .catch((error) => {
+      console.log(error.message);
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+};
